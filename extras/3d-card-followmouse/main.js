d6484f55d43c50a2237e0a5f044d2e8e34ecc653
Eduardo Berzunza <eduardoberzunzal@gmail.com>
2024-02-27 13:24:55 -0600

    New cards

---
+++ extras/3d-card-followmouse/main.js
@@ -0,0 +1,39 @@
+const pre = document.querySelector("pre");
+const container = document.querySelector('.container');
+
+container.addEventListener("mousemove", (e) => {
+    console.log('mouse');
+  rotateElement(e, pre);
+  
+});
+
+container.addEventListener("mouseleave", (e) => {
+    e.stopPropagation();
+    stopElement(pre);
+})
+
+const rotateElement = (event, element) => {
+
+  const x = event.clientX;
+  const y = event.clientY;
+
+  const middleX = window.innerWidth / 2;
+  const middleY = window.innerHeight / 2;
+
+  const offsetX = ((x - middleX) / middleX) * 45;   
+  const offsetY = ((y - middleY) / middleY) * 45;
+  const delay = '0s';
+
+  element.style.setProperty("--rotateX", offsetX + "deg");
+  element.style.setProperty("--rotateY", -1 * offsetY + "deg");
+  
+  element.style.setProperty("--delay", delay);
+}
+
+const stopElement = (element) => {
+    element.style.setProperty("--rotateX", "0deg");
+    element.style.setProperty("--rotateY", "0deg");
+    element.style.setProperty("--delay", '.5s');
+}
+
+
